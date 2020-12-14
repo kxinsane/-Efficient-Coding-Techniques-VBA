1dea24c67e78352e3ad133456922a94dbd786f82
Karol Jeziorowski <61558496+kxinsane@users.noreply.github.com>
2020-12-14 18:22:30 +0100

    Update RemovingDuplicates.vb

--- RemovingDuplicates.vb
+++ RemovingDuplicates.vb
@@ -1,14 +1,14 @@
-    ' using an Offset method to exclude Headers, Range must be contiguous because blanks will stop the process
+' using an Offset method to exclude Headers, Range must be contiguous because blanks will stop the process
     Range("A1").CurrentRegion.Offset(1, 0).RemoveDuplicates (Array(1))
     
-	' using short notation and include or exclude Headers - it will cover Entire Column A
+' using short notation and include or exclude Headers - it will cover Entire Column A
     [A:A].RemoveDuplicates Columns:=1, Header:=xlNo
     [A:A].RemoveDuplicates Columns:=1, Header:=xlYes
 
-	' will cover Entire Column A with include Header
+' will cover Entire Column A with include Header
     Columns(1).RemoveDuplicates Columns:=1
     [A:A].RemoveDuplicates Columns:=1
 
-	' Range, using Short Notation and include or exclude Headers - it will cover the Entire Column A
-	' it will remove duplicates of any Rows that are the same for the first 4 Columns, not duplicates anywhere in the Range
+' Range, using Short Notation and include or exclude Headers - it will cover the Entire Column A
+' it will remove duplicates of any Rows that are the same for the first 4 Columns, not duplicates anywhere in the Range
     ActiveSheet.Range("A:D").RemoveDuplicates Columns:=Array(1, 2, 3, 4), Header:=xlYes
