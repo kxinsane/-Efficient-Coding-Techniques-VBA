1d707fea353fe13a27c09f4a61227b084e9eeb52
kxinsane <karol.jeziorowski@interia.pl>
2020-12-14 19:08:43 +0100

    -Efficient-Coding-Techniques-VBA

---
+++ Get Column Letter And Row Number From Both Parts Of A Range/GetColLetterRowNumberFromRange.vb
@@ -0,0 +1,14 @@
+	' Select a Range for the Examples
+	Range("D2:L23").Select
+
+	' identify Column from the 1st part of the Range following the ":"
+	Debug.Print Split(Split(Selection.Address, ":")(0), "$")(1)
+
+	' identify Row from the first part of the Range following the ":"
+	Debug.Print Split(Split(Selection.Address, ":")(0), "$")(2)
+
+	' identify Column from the 2nd part of the Range following the ":"
+	Debug.Print Split(Split(Selection.Address, ":")(1), "$")(1)
+
+	' identify Row from the 2nd part of the Range following the ":"
+	Debug.Print Split(Split(Selection.Address, ":")(1), "$")(2)
