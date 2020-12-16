b73d474a20b17c3f217e2c3e4a2826c22b2d85c1
kxinsane <karol.jeziorowski@interia.pl>
2020-12-16 19:18:18 +0100

    Create WaitingPausing.vb

---
+++ Waiting or Pausing/WaitingPausing.vb
@@ -0,0 +1,7 @@
+	' Pause 5 seconds:
+	Application.Wait (Now + TimeValue("0:00:06"))
+
+	' Pause until a specific time ie. this will pause a macro until 9:00am
+	Application.Wait "09:00:00"
+
+	'Wait doesn't accept delays less than 1 second
