fd51228066da2e57876c9967d5deeeb1c573bfbe
KXInsane <61558496+kxinsane@users.noreply.github.com>
2021-01-18 19:33:22 +0100

    Ways to replacing zero or negative values

--- Replacing Zero or Negative values in Range, Cells or Defined Name with Blanks/ReplacingZeroNegativeValuesInRange.vb
+++ Replacing Zero or Negative values in Range, Cells or Defined Name with Blanks/ReplacingZeroNegativeValuesInRange.vb
@@ -1,36 +1,36 @@
-	' Columns, default replace zero ("0") with blanks
-	Columns("A:D").Replace What:="0", Replacement:=""
+' Columns, default replace zero ("0") with blanks
+Columns("A:D").Replace What:="0", Replacement:=""
 
-	' Search for containing zero - will replace 0 and also replace 20, 200, 202 etc.
-	Columns("A:D").Replace What:="*0*", Replacement:=""
+' Search for containing zero - will replace 0 and also replace 20, 200, 202 etc.
+Columns("A:D").Replace What:="*0*", Replacement:=""
 
-	' Columns, default replace negatives with zero (0) with uses a Minus sign
-	Columns("I:I").Replace What:="-*", Replacement:="0"
+' Columns, default replace negatives with zero (0) with uses a Minus sign
+Columns("I:I").Replace What:="-*", Replacement:="0"
 
-	' Look at whole Cells, Match Case and Replace.
-	Columns("A:D").Replace What:="0", Replacement:="", LookAt:=xlWhole, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+' Look at whole Cells, Match Case and Replace.
+Columns("A:D").Replace What:="0", Replacement:="", LookAt:=xlWhole, SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
 
-	' Rows, look at whole Cells
-	Rows("1:5").Replace What:="0", Replacement:="", LookAt:=xlWhole
+' Rows, look at whole Cells
+Rows("1:5").Replace What:="0", Replacement:="", LookAt:=xlWhole
 
-	' Range, simple, replace all zeros (0) with Blanks
-	Range("A1:B4").Replace What:="0", Replacement:=""
+' Range, simple, replace all zeros (0) with Blanks
+Range("A1:B4").Replace What:="0", Replacement:=""
 
-	' Defined Name or Named Range
-	[MyRange].Replace What:="0", Replacement:="", LookAt:=xlWhole
+' Defined Name or Named Range
+[MyRange].Replace What:="0", Replacement:="", LookAt:=xlWhole
 
-	' Short Notation
-	[A1:D5].Replace What:="0", Replacement:=""
+' Short Notation
+[A1:D5].Replace What:="0", Replacement:=""
 
-	' Using With for the ActiveSheet in Workbook
-	With ThisWorkbook.ActiveSheet.Cells
-		.Replace What:="0", Replacement:=""
-	End With
+' Using With for the ActiveSheet in Workbook
+With ThisWorkbook.ActiveSheet.Cells
+	.Replace What:="0", Replacement:=""
+End With
 
-	' Using With for Columns
-	With Columns("A:D").Cells
-		.Replace What:="0", Replacement:=""
-	End With
+' Using With for Columns
+With Columns("A:D").Cells
+	.Replace What:="0", Replacement:=""
+End With
 
-	' Replacing Errors
-	.Replace What:="#N/A", Replacement:="", LookAt:=xlWhole
+' Replacing Errors
+.Replace What:="#N/A", Replacement:="", LookAt:=xlWhole
