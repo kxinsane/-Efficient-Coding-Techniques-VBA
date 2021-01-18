26dbe828efa0f155315a1b05a5c4fe5d31ab8183
KXInsane <61558496+kxinsane@users.noreply.github.com>
2021-01-18 19:31:59 +0100

    Ways to delete the last Row in Column

--- Deleting Last Row In Range/DeletingLastRowInRange.vb
+++ Deleting Last Row In Range/DeletingLastRowInRange.vb
@@ -1,7 +1,7 @@
-	' delete the last Row in Column 1 ("A")
-	With Activesheet
-		.Cells(Rows.Count, 1).End(xlUp).EntireRow.Delete
-	End with
+' delete the last Row in Column 1 ("A")
+With Activesheet
+	.Cells(Rows.Count, 1).End(xlUp).EntireRow.Delete
+End with
 
-	' delete the last Row in Column 2 ("B")
-	Me.Cells(Rows.Count, 2).End(xlUp).EntireRow.Delete
+' delete the last Row in Column 2 ("B")
+Me.Cells(Rows.Count, 2).End(xlUp).EntireRow.Delete
